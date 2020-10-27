bc2dd2cd2fdbd72521d901345132fad9778a8f28
hyngwar <seokkjj83@gmail.com>
2020-10-27 18:51:28 +0100

    comandos realizados, provision.sh actualizado y todo funciona despues del destroy

--- provision.sh
+++ provision.sh
@@ -1,3 +1,10 @@
 sudo apt-get -y update
 sudo apt-get -y install nginx
+
+cp -r /usr/share/nginx/* /vagrant/
+rm -rf /usr/share/nginx/*
+
+cd /usr/share/nginx
+sudo ln -s /vagrant/www
+
 sudo service nginx start
