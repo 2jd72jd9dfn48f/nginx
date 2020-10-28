3806305b2b031188c71fbacb67ee39dc32d6836b
2jd72jd9dfn48f <57388998+2jd72jd9dfn48f@users.noreply.github.com>
2020-10-28 10:08:56 +0100

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,7 +1,7 @@
 sudo apt-get -y update
 sudo apt-get -y install nginx
 
-rm -rf /etc/nginx/sites-enabled
-cp -R /vagrant/sites-enabled /etc/nginx/ 
+#rm -rf /etc/nginx/sites-enabled
+#cp -R /vagrant/sites-enabled /etc/nginx/  
 
 sudo service nginx start
